209566232e55b4c7364d3c555a3e5fd4f8fefbef
Mifa43 <mifa43kotez@gmail.com>
2022-05-27 21:59:17 +0200

    dodata home ruta

--- foodApp/urls.py
+++ foodApp/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from foodRecipes.views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', HomeView.as_view(), name="home"),
 ]
