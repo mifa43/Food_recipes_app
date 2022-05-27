17ea2b3698e564c923b5398bad5dd05cb1e4c2e2
Mifa43 <mifa43kotez@gmail.com>
2022-05-27 21:58:34 +0200

    host i app config izmnjeni

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q28k2j5jilr$duwv+j^og63q=+t(k&-40o^$8h*uftrdivt82(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodRecipes.apps.FoodrecipesConfig'
 ]
 
 MIDDLEWARE = [
