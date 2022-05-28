86755dfafdef87192d41712b1f338c5233501bbe
Mifa43 <mifa43kotez@gmail.com>
2022-05-28 21:44:57 +0200

    kreiran model za elastic

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Recipes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes", null=True)
+    title = models.CharField(max_length=255)
+    ingredient = models.TextField()
+    recipe = models.TextField()
+    author = models.CharField(default=User,max_length=255)
+    rating = models.IntegerField()
+
+    def __str__(self):
+            return self.title
