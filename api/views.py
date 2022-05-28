ea4d8564933a5176151528f0f2b72b36f85c7018
Mifa43 <mifa43kotez@gmail.com>
2022-05-28 21:43:50 +0200

    kreiran view sa get metodom  za recepte

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.models import Recipes
+from api.serializers import RecipesSerializer
 
 # Create your views here.
+
+
+@api_view(["GET"])  # kao i u fastapi deklaracija metode
+def getRecipes(request):    
+    if request.method == 'GET':
+        rec = Recipes.objects.all() 
+    serializer = RecipesSerializer(rec, many=True)
+    return Response(serializer.data)
