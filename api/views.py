7933ecc90395bad30062d540f51a86255dd26b06
Mifa43 <mifa43kotez@gmail.com>
2022-05-29 10:47:29 +0200

    kreiran api koji prihvata post metod-request body i csrf token

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
+from turtle import title
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.models import Recipes
 from api.serializers import RecipesSerializer
-
+from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 
 
@@ -13,3 +14,24 @@ def getRecipes(request):
         rec = Recipes.objects.all() 
     serializer = RecipesSerializer(rec, many=True)
     return Response(serializer.data)
+
+@csrf_protect
+@api_view(["POST"])  # kao i u fastapi deklaracija metode
+def postRecipes(request):    
+    if request.method == 'POST':
+        rec = RecipesSerializer(Recipes(), request.data)    # |TypeError: Model.save() missing 1 required positional argument: 'self'|*Recipes() reseno sa
+        if rec.is_valid(raise_exception=True):  # validacija request.data
+            rec.save()  #cuvanje u elastic
+
+        return Response(rec.data)  
+
+
+# {
+#     "title": "asfasf",
+#     "ingredient": "bbbb", 
+#     "recipe": "asfadfgcvb", 
+#     "author":"sfxbbb",
+#     "rating":1
+
+# }
+# request body
