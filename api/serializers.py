eb7f18ebb2bca3d3af05648f0b41fea73a5da283
Mifa43 <mifa43kotez@gmail.com>
2022-05-29 10:45:50 +0200

    dodato default polje null

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Recipes
+from django.contrib.auth.models import User
+from rest_framework.fields import CurrentUserDefault
 
+# u fastapi-u kao pydantic modeli
 class RecipesSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(max_length=200)
+    user = CurrentUserDefault()
     title = serializers.CharField(max_length=200)
     ingredient = serializers.CharField()
     recipe = serializers.CharField()
