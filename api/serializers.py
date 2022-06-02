7611850498338483815020fcb3aff6ac059d4be5
Mifa43 <mifa43kotez@gmail.com>
2022-06-02 21:49:58 +0200

    kreiran serializer model

--- api/serializers.py
+++ api/serializers.py
@@ -15,3 +15,17 @@ class RecipesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipes
         fields = ("__all__")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
