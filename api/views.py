5b44e057b9a8bcb1d5477ada768055b45c52c473
Mifa43 <mifa43kotez@gmail.com>
2022-06-02 20:27:47 +0200

    vracamo request body

--- api/views.py
+++ api/views.py
@@ -1,12 +1,11 @@
-from turtle import title
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.models import Recipes
-from api.serializers import RecipesSerializer
+from api.serializers import RecipesSerializer, UserSerializer
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import redirect
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -25,16 +24,16 @@ def postRecipes(request):
         rec = RecipesSerializer(Recipes(), request.data)    # |TypeError: Model.save() missing 1 required positional argument: 'self'|*Recipes() reseno sa
         if rec.is_valid(raise_exception=True):  # validacija request.data
             rec.save()  #cuvanje u elastic
-            return redirect('home')
+            return Response(rec.data)#redirect('home')
 
         return Response(rec.data)  
 
-# {
+
+# { 
 #     "title": "asfasf",
 #     "ingredient": "bbbb", 
 #     "recipe": "asfadfgcvb", 
 #     "author":"sfxbbb",
 #     "rating":1
-
 # }
 # request body
