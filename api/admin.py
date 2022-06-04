24ca5504bd45f3fbced5854b1e537d859dbd6b87
Mifa43 <mifa43kotez@gmail.com>
2022-06-04 18:47:44 +0200

    registrovan novi model

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Recipes
+from .models import Recipes, ClearBitData
 # Register your models here.
 
 admin.site.register(Recipes)
+admin.site.register(ClearBitData)
