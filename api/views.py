7f460dcc76419ab1693e1d68678abaae414c1409
Mifa43 <mifa43kotez@gmail.com>
2022-06-04 15:48:48 +0200

    kreiranje nove rute koja ce da gadja clearbit api i upise informacijeu db

--- api/views.py
+++ api/views.py
@@ -2,12 +2,13 @@ from cgitb import handler
 from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from api.models import Recipes
-from api.serializers import RecipesSerializer, UserSerializer
+from api.models import Recipes, ClearBitData
+from api.serializers import RecipesSerializer, UserSerializer, ClearBitSerializer
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 import requests
+from requests.auth import HTTPBasicAuth
 # Create your views here.
 
 
@@ -46,6 +47,28 @@ def register(request):
         else:
             return Response({"email": "is not valid"})
         return Response(reg.data )
+
+@csrf_protect
+@api_view(["POST"])
+def clearBitApi(request):
+    if request.method == 'POST':
+        method = "get"
+        url = "https://person-stream.clearbit.com/v1/people/email/alex@alexmaccaw.com"
+        token = "-u sk_990572798c73dacbc19933426c0ff168:"
+        headers = {'Authorization': 'Token sk_990572798c73dacbc19933426c0ff168:'}
+        hed={"username": "sk_990572798c73dacbc19933426c0ff168:"}
+        rsp = requests.request(method, url, headers=headers, auth=hed)
+        print(rsp.content)
+        # data = ClearBitSerializer(ClearBitData(), request.data)
+        # getData = requests.post("https://person-stream.clearbit.com/v1/people/email/alex@alexmaccaw.com -u sk_990572798c73dacbc19933426c0ff168:")
+        # print(getData.content)
+        # if data.is_valid(raise_exception=True):
+    
+        #     data.save()
+        #     return Response(data.data )
+        # else:
+        #     return Response({"email": "is not valid"})
+        return Response(rsp )
 # { 
 #     "title": "asfasf",
 #     "ingredient": "bbbb", 
@@ -64,3 +87,6 @@ def register(request):
 # {"username": "adawfrgsdegts34", 
 # "email": "patrick@stripe.com",
 #  "password":  "kskslfk224"}
+
+# sk_990572798c73dacbc19933426c0ff168 streetAddress foundedYear sectordomain location linkedin employees estimatedAnnualRevenue 
+
