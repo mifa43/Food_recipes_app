8129c3611c78704e6cf31459ac7da860dd73fb61
Mifa43 <mifa43kotez@gmail.com>
2022-06-05 22:54:30 +0200

    kreiran view za registraciju

--- foodRecipes/views.py
+++ foodRecipes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 from django.shortcuts import render
 import requests
 from rest_framework.response import Response
 from api.models import Recipes
+from api.serializers import UserSerializer
+from foodRecipes.forms import RegisterForm
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -13,3 +15,8 @@ class GetRecipe(ListView):
     model = Recipes
     context_object_name = "recipe"
     template_name = "main/getRecipes.html"
+
+class Register(CreateView):
+    form_class = RegisterForm
+    template_name = "main/register/register.html"
+    # success_url = reverse_lazy("home")
