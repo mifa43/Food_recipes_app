24ae44dba1ac4198d2b5352959e6293f9510f53a
Mifa43 <mifa43kotez@gmail.com>
2022-06-05 15:59:41 +0200

    izbacena greska

--- api/views.py
+++ api/views.py
@@ -158,8 +158,3 @@ def searchRecipe(request):
                     "author": hit.author,
                     "rating": hit.rating
                 })
-<<<<<<< HEAD
-    return Response("search recipes, ingredients and title")  
-=======
-    return Response("search recipes, ingredients and title")  
->>>>>>> d40a33e1b1da9d9fe0ca7adb4b327477ec8f0e07
